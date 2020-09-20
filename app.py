9c0fbcdfddb78c186084da7150ee1638f073c2df
Mostafa <54817213+arknfel@users.noreply.github.com>
2020-09-20 14:42:44 +0200

    Major refactoring, logic and functionality enhancements

--- app.py
+++ app.py
@@ -4,47 +4,15 @@ from datetime import datetime, timedelta
 from flask import Flask, jsonify, make_response
 
 
-# Initializing the flask app
-app = Flask(__name__)
+# Remap algorithm
+# The function will iterate over a a list of repositories
+# while assembling a dict of languages, each language has a sub-dict
+# of the number of repositories and a list of those repositories names 
+def remap(repos):
 
-# Setting the IP/site and port for the host of
-# the flask microservice 
-HOST, PORT = '0.0.0.0', 3000
+    langs = {} # algorithm output data
 
-# Microservice worker
-@app.route("/api/v1/trends/", methods=['GET'])
-def trends():
-
-    date = (datetime.now() - timedelta(30)).isoformat()
-
-    url = f"https://api.github.com/search/repositories?q=created:>{date}&sort=stars&order=desc"
-
-    res = requests.get(url) # github's response
-
-    # Handles and returns non-ok responses from github
-    if res.status_code != 200:
-
-        payload = {
-            'requested_url': url,
-            'response_status_code': res.status_code,
-            'resonse_content': res.json()
-        }
-
-        payload = json.dumps(payload, indent=4, sort_keys=False)
-        msrv_res = make_response(payload)
-        msrv_res.mimetype = 'application/json'
-        return msrv_res
-
-    repos = res.json()['items']  # a list of repositories in github's json response
-
-    # if there are available items returned from the github request,
-    # proceed to the remapping algorithm, else, notate that no data were found.
-    if len(repos) > 0:
-
-        # Remapping algorithm
-
-        langs = {} # algorithm output data
-
+    if repos:
         for repo in repos:
             # for index readability
             lang = str(repo['language'])
@@ -62,21 +30,55 @@ def trends():
                     'usage': 1,
                     'repositories': [repo_name]
                 }
+    return langs
 
-        # minimal tweaks to flask's default response that holds
-        # the JSON object of dict 'langs'
-        msrv_res = make_response(json.dumps(langs,indent=4, sort_keys=False))
-        msrv_res.mimetype = 'application/json'
-        
-        # returns a valid JSON object
-        return msrv_res
-    
-    # len(repos) == 0 (if repos had no elements)
-    # returns a JSON object of the url and a warning message
+
+# Initializing the flask app
+app = Flask(__name__)
+
+# setting the IP/site and port for the host of
+# the flask microservice 
+HOST, PORT = '127.0.0.1', 3000
+
+
+# Microservice end-point worker
+@app.route("/api/v1/trends/", methods=['GET'])
+def trends():
+
+    date = (datetime.now() - timedelta(30)).isoformat()  # The date of 30 days ago
+
+    url = f"https://api.github.com/search/repositories?q=created:>{date}&sort=stars&order=desc"  # Target url
+
+    res = requests.get(url) # Github's response
+
+    payload = None
+
+    # Handling the response from GitHub
+    #
+    # if the response is 'ok', call remap() to
+    # assemble the output
+    if res.status_code == 200:
+        repos = res.json()['items']
+        payload = remap(repos)
+    #
+    # else, forward Github's response as the microservice's output
     else:
-        return jsonify(url=url, error="No repositories were returned")
+        payload = {
+            'requested_url': url,
+            'response_status_code': res.status_code,
+            'response_content': res.json()
+        }
+
+    # Customising the flask response to:
+    # 1. Preserve the original order of dicts keys after jsonify.
+    # 2. Pretty-printing the output json for user readability.
+    msrv_res = make_response(json.dumps(payload, indent=4, sort_keys=False))
+    msrv_res.mimetype = 'application/json'
+
+    return msrv_res
 
 
 # Main Thread starts here
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
+
